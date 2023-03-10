442c5b97ae5d8b1cfef8ad0cf31377a81cc4eadd
Bruno Duarte <brunoduarte.inf@outlook.com>
2023-03-09 21:02:59 -0300

    Feat

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const gitToken = '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';
+const gitToken = process.env.REACT_APP_AUTH_KEY
 
 const api =  axios.create({
     baseURL: 'https://api.github.com/',
